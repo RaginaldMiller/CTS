ac12ee120aeb2c137fd2cbf07069071f5c3c51fc
ReginaldMiller <10154057907@qq.com>
2015-11-08 18:55:15 +0800

    test

---
+++ TestCTS/bin/classes.dex.d
@@ -0,0 +1,4 @@
+E:\Users\Administrator\workspace\TestCTS\bin\classes.dex : \
+E:\Users\Administrator\workspace\TestCTS\bin\classes\com\dg11185\test\CtsHelper.class \
+E:\Users\Administrator\workspace\TestCTS\bin\classes\com\dg11185\test\Demo1.class \
+E:\Users\Administrator\workspace\TestCTS\bin\classes\com\dg11185\test\UiAutomatorHelper.class \
